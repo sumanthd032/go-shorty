3ebb6a3a1b172b6ec08e102fa1a4b59299fbea8e
sumanthd032 <sumanthd032@gmail.com>
2025-08-24 16:05:46 +0530

    remove unwanted files

--- render-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# This script runs database migrations and then starts the main server.
-
-# Run migrations
-/goose -dir "migrations" postgres "$DATABASE_DSN" up
-
-# Start the server
-/server
